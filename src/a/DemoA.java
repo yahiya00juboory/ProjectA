b11872556d56b844dd91122a8d7642d1d88549e1
Yehyia Aljuboory <Yehyia Aljuboory@DESKTOP-JNNUE2G>
2024-01-03 20:30:22 -0600

    implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -5,4 +5,11 @@ public class DemoA {
 	public static void main(String[] args) {
 		System.out.println("A");
 	}
+	
+	
+	//Sprint 2 Automation
+	public static void add (int a, int b) {
+		System.out.println(a+b);
+	}
+	
 }
